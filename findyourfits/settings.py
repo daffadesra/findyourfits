8a395342b8af21f6b09f9e9773cca31ac8332365
daffadesra <desrad81@gmail.com>
2024-09-18 00:30:39 +0700

    tambah fitur auto push PWS

--- findyourfits/settings.py
+++ findyourfits/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://daffa-desra-findyourfits.pbp.cs.ui.ac.id/", "https://daffa-desra-findyourfits.pbp.cs.ui.ac.id/"]
+
+
+
