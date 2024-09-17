770b33536131de930a4c4c13ef189bfc19dca8c0
daffadesra <desrad81@gmail.com>
2024-09-18 00:12:13 +0700

    Menambahkan fitur show xml dan json by product id

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, sell_product_entry, show_xml, show_json
+from main.views import show_main, sell_product_entry, show_xml, show_json, show_json_by_id, show_xml_by_id
 
 app_name = 'main'
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('sell-product-entry', sell_product_entry, name='sell_product_entry'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -32,4 +32,11 @@ def show_xml(request):
 def show_json(request):
     data = ProductEntry.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-    
+
+def show_xml_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
