8a265e63ab08d12dac41aa9f199cc352991c341c
daffadesra <desrad81@gmail.com>
2024-09-23 13:22:40 +0700

    Membuat fungsi login dan register

--- main/views.py
+++ main/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.core import serializers
 from main.forms import ProductEntryForm
 from main.models import ProductEntry
+# Tugas 4
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def show_main(request):
     product_entries = ProductEntry.objects.all()
@@ -40,3 +45,28 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = ProductEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun anda berhasil dibuat!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
