1ec12423962c9d03e9b81f4a02ef62fefbb49851
daffadesra <desrad81@gmail.com>
2024-11-17 19:00:34 +0700

    make create_product_flutter view

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from main.views import show_main, sell_product_entry, show_xml, show_json, show_json_by_id, show_xml_by_id
 from main.views import register, login_user, logout_user, edit_product, hapus_product, add_product_ajax
+from main.views import create_product_flutter
 
 
 app_name = 'main'
@@ -18,4 +19,5 @@ urlpatterns = [
     path('edit-product/<uuid:id>', edit_product, name='edit_product'),
     path('delete/<uuid:id>', hapus_product, name='hapus_product'),
     path('add-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-product-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
